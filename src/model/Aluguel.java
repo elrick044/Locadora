dd06f0262bf8916d5dcf31b2fcf9b23aa904977a
elrick046 <87386997+elrick046@users.noreply.github.com>
2024-06-03 23:12:04 -0300

    Commit 02.3 - Implementação de classes

--- src/model/Aluguel.java
+++ src/model/Aluguel.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.Date;
+
 public class Aluguel {
+    private int id;
+    private Cliente cliente;
+    private Midia midia;
+    private Date dataAluguel;
+    private Date DueDate;
+    private Date dataDevolucao;
+    private double preco;
+
+    //private bool IsReturned => ReturnDate.HasValue;
 }
