008bba24c0771ba34c19501a00c9ae4837ca58a7
Jaaum32 <jv418070@gmail.com>
2024-06-04 00:58:22 -0300

    Commit 04.1 — Correção na Factory

--- src/factory/MediaFactory.java
+++ src/factory/MediaFactory.java
@@ -10,5 +10,6 @@ import java.util.List;
 public class MediaFactory {
     public static Midia createMedia(int id, String titulo, List<Genero> genero, Categoria categoria, Estado estado){
         return new Midia(id, titulo, genero, categoria, estado);
+        //arrumei
     }
 }
